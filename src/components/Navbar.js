c62e990e7fb103d6414191c389b588e390a77e9b
manasjadhav <manasjjadhav@gmail.com>
2022-10-30 12:43:07 +0530

    done with active link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,64 @@
 import React from "react";
+import { useState } from "react";
 import companyLogo from "../images/logo/companyLogo.png";
 import magnifyingGlass from "../images/logo/magnifying-glass.png";
+
 export default function Navbar() {
+  const active =
+    "bg-default-pink-hover px-2 py-1 cursor-pointer hover:scale-110 rounded-md text-white";
+  const NotActive = "cursor-pointer hover:scale-110";
+  const [Active, setActive] = useState(1);
+
+  function ActiveLink(id) {
+    setActive(id);
+  }
+
   return (
     <div className="flex justify-evenly text-heading">
       <ul className="flex space-x-8 items-center mt-10 mr-4">
         <li>
           <img src={companyLogo} alt="logo" />
         </li>
-        <li className="cursor-pointer hover:scale-110">Popular</li>
-        <li className="bg-default-pink-hover px-4 py-1 rounded-md text-white cursor-pointer hover:scale-110">
+        <li
+          onClick={() => {
+            ActiveLink(1);
+          }}
+          className={Active === 1 ? active : NotActive}
+        >
+          Popular
+        </li>
+        <li
+          onClick={() => {
+            ActiveLink(2);
+          }}
+          className={Active === 2 ? active : NotActive}
+        >
           New
         </li>
-        <li className="cursor-pointer hover:scale-110">Reading list</li>
-        <li className="cursor-pointer hover:scale-110">Topics</li>
-        <li className="cursor-pointer hover:scale-110">Subscribe</li>
+        <li
+          onClick={() => {
+            ActiveLink(3);
+          }}
+          className={Active === 3 ? active : NotActive}
+        >
+          Reading list
+        </li>
+        <li
+          onClick={() => {
+            ActiveLink(4);
+          }}
+          className={Active === 4 ? active : NotActive}
+        >
+          Topics
+        </li>
+        <li
+          onClick={() => {
+            ActiveLink(5);
+          }}
+          className={Active === 5 ? active : NotActive}
+        >
+          Subscribe
+        </li>
         <li>
           <input
             className="relative border-default-black left-22 border p-2 rounded-md w-64 shadow-sm"
